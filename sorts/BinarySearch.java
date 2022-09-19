d8b41a2eb2caa6aa07d82c4d41871789ee6afe6a
PoroPike <113169553+PoroPike@users.noreply.github.com>
2022-09-19 18:44:52 +0800

    二分查找

--- sorts/BinarySearch.java
+++ sorts/BinarySearch.java
@@ -1,6 +1,6 @@
 public class BinarySearch {
     public static void main(String[] args) {
-        int[] array = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9};
+        int[] array = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9};//
         System.out.println(search(array, 5));
     }
 
